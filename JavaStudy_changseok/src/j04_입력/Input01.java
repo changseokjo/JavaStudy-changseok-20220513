c3663b6395ffc86e73e506851a8d1243ad8f2e64
changseokjo <yulini1022@gmail.com>
2022-10-20 04:59:20 +0900

    입력 / 조건 수업코드

---
+++ JavaStudy_changseok/src/j04_입력/Input01.java
@@ -0,0 +1,23 @@
+package j04_입력;
+
+import java.util.Scanner;
+
+public class Input01 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("값 입력1: ");
+		int num1 = scanner.nextInt();
+		System.out.print("값 입력2: ");
+		int num2 = scanner.nextInt();
+		
+		System.out.println("입력한 값: " + (num1 + num2));
+		
+		scanner.next();				//문자열(String)
+		scanner.nextLine();			//문자열(String)
+		scanner.next().charAt(0);	//문자(char)
+		scanner.nextInt();			//정수(int)
+		scanner.nextDouble();		//실수(double)
+	}
+
+}
---
+++ JavaStudy_changseok/src/j04_입력/Input02.java
@@ -0,0 +1,18 @@
+package j04_입력;
+
+public class Input02 {
+
+	public static void main(String[] args) {
+		/*
+		 * 문자열1 입력: nextLine()			hi
+		 * 문자열2 입력: next()				안녕
+		 * 문자 입력: next().charAt(0)		A
+		 * 정수 입력: nextInt()				10
+		 * 실수 입력: nextDouble()			3.14
+		 * 
+		 */
+
+		
+	}
+
+}
