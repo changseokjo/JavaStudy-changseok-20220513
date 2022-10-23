b5e7ea3c7e7d3d7aa908e785a56825d9f951236b
changseokjo <yulini1022@gmail.com>
2022-10-24 05:03:53 +0900

    람다식 및 예외처리 수업 코드

---
+++ JavaStudy_changseok/src/j23_예외처리/ThrowEx.java
@@ -0,0 +1,25 @@
+package j23_예외처리;
+
+import javax.management.RuntimeErrorException;
+
+public class ThrowEx {
+
+	public static void main(String[] args) {
+		
+		try {
+			System.out.println("프로그램 실행중...");
+			int i = 0;
+			
+			if(i == 0) {
+				throw new ValidationCustomException("예외 발생");
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			System.out.println("무조건 실행");
+		}
+
+	}
+
+}
